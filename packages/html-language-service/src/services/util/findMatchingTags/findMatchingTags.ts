3a7e9c1fd4e69c4d82cd79ab1168ce58839934f8
Simon Siefke <simon.siefke@gmail.com>
2019-11-13 18:40:43 +0100

    fix: find matching tags not working when there are tags in between

--- packages/html-language-service/src/services/util/findMatchingTags/findMatchingTags.ts
+++ packages/html-language-service/src/services/util/findMatchingTags/findMatchingTags.ts
@@ -1,108 +1,115 @@
 import {
   createScanner,
   ScannerState,
-  TokenType,
-} from '@html-language-features/html-parser'
-import { isSelfClosingTag } from '../../../Data/Data'
+  TokenType
+} from "@html-language-features/html-parser";
+import { isSelfClosingTag } from "../../../Data/Data";
 import {
   getNextClosingTag,
-  getPreviousOpeningTagName,
-} from '../getParentTagName'
+  getPreviousOpeningTagName
+} from "../getParentTagName";
+import { getNextClosingTagName } from "../getNextClosingTagName";
 
 export type MatchingTagResult =
   | {
-      type: 'startAndEndTag'
-      tagName: string
-      startTagOffset: number
-      endTagOffset: number
+      type: "startAndEndTag";
+      tagName: string;
+      startTagOffset: number;
+      endTagOffset: number;
     }
   | {
-      type: 'onlyStartTag'
-      tagName: string
-      startTagOffset: number
+      type: "onlyStartTag";
+      tagName: string;
+      startTagOffset: number;
     }
   | {
-      type: 'onlyEndTag'
-      tagname: string
-      endTagOffset: number
-    }
+      type: "onlyEndTag";
+      tagname: string;
+      endTagOffset: number;
+    };
 
 export const findMatchingTags: (
   text: string,
   offset: number
 ) => MatchingTagResult | undefined = (text, offset) => {
-  const scanner = createScanner(text, { initialOffset: offset })
-  scanner.stream.goBackToUntilEitherChar('<', '>')
-  const char = scanner.stream.peekLeft(1) //?
-  if (char === '<') {
-    if (scanner.stream.nextChars(2) === '--') {
-      return undefined
+  const scanner = createScanner(text, { initialOffset: offset });
+  scanner.stream.goBackToUntilEitherChar("<", ">");
+  const char = scanner.stream.peekLeft(1); //?
+  if (char === "<") {
+    if (scanner.stream.nextChars(2) === "--") {
+      return undefined;
     }
-    if (scanner.stream.peekRight() === '/') {
-      const endTagOffset = scanner.stream.position //?
-      scanner.stream.advance(1)
-      scanner.state = ScannerState.AfterOpeningEndTag
-      scanner.scan()
-      const tagName = scanner.getTokenText()
-      scanner.stream.position //?
+    if (scanner.stream.peekRight() === "/") {
+      const endTagOffset = scanner.stream.position; //?
+      scanner.stream.advance(1);
+      scanner.state = ScannerState.AfterOpeningEndTag;
+      scanner.scan();
+      const tagName = scanner.getTokenText();
+      scanner.stream.position; //?
       const previousOpenTagName = getPreviousOpeningTagName(
         scanner,
         endTagOffset - 2
-      )
+      );
       if (!previousOpenTagName || tagName !== previousOpenTagName.tagName) {
-        return undefined
+        return undefined;
       }
       return {
-        type: 'startAndEndTag',
+        type: "startAndEndTag",
         tagName,
         startTagOffset: previousOpenTagName.offset - 1,
-        endTagOffset: endTagOffset - 1,
-      }
+        endTagOffset: endTagOffset - 1
+      };
     } else {
-      const startTagOffset = scanner.stream.position - 1
-      scanner.state = ScannerState.AfterOpeningStartTag
-      const token = scanner.scan()
+      const startTagOffset = scanner.stream.position - 1;
+      scanner.state = ScannerState.AfterOpeningStartTag;
+      const token = scanner.scan();
       if (token !== TokenType.StartTag) {
-        return undefined
+        return undefined;
       }
-      const tagName = scanner.getTokenText()
+      const tagName = scanner.getTokenText();
       if (isSelfClosingTag(tagName)) {
         return {
-          type: 'onlyStartTag',
+          type: "onlyStartTag",
           tagName,
-          startTagOffset,
-        }
+          startTagOffset
+        };
       }
-      scanner.stream.advanceUntilChar('>')
-      scanner.stream.advance(1)
-      const nextClosingTag = getNextClosingTag(scanner, scanner.stream.position)
+      scanner.stream.advanceUntilChar(">");
+      scanner.stream.advance(1);
+      const nextClosingTag = getNextClosingTagName(
+        scanner,
+        scanner.stream.position
+      );
       if (!nextClosingTag || nextClosingTag.tagName !== tagName) {
         return {
-          type: 'onlyStartTag',
+          type: "onlyStartTag",
           tagName,
-          startTagOffset,
-        }
+          startTagOffset
+        };
       }
       return {
-        type: 'startAndEndTag',
+        type: "startAndEndTag",
         tagName,
         startTagOffset,
-        endTagOffset: nextClosingTag.offset - 2,
-      }
+        endTagOffset: nextClosingTag.offset - 2
+      };
     }
-  } else if (char === '>') {
+  } else if (char === ">") {
     if (offset >= scanner.stream.position) {
-      return undefined
+      return undefined;
     }
-    scanner.stream.goBack(2)
-    scanner.stream.goBackToUntilEitherChar('<', '>')
-    const char = scanner.stream.peekLeft(1) //?
-    if (char === '>') {
-      return undefined
+    scanner.stream.goBack(2);
+    scanner.stream.goBackToUntilEitherChar("<", ">");
+    const char = scanner.stream.peekLeft(1); //?
+    if (char === ">") {
+      return undefined;
     }
-    return findMatchingTags(scanner.stream.getSource(), scanner.stream.position)
+    return findMatchingTags(
+      scanner.stream.getSource(),
+      scanner.stream.position
+    );
   }
-  return undefined
-}
+  return undefined;
+};
 
-findMatchingTags('<a>a</a>', 0) //?
+findMatchingTags("<a>a</a>", 0); //?
